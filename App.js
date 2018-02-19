6880f17f546fb3f3295e86e8bb05d27388d45473
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-18 23:43:56 -0500

    Firebase Client Setup

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import firebase from 'firebase'
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+} from 'react-native-dotenv'
 import { Header } from './src/components/common'
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DATABASE_URL,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    })
+  }
+
   render() {
     return (
       <View>
